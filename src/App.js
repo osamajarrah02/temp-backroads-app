78b275e486c6275b5bb48d65bd8f7db6d1102d6f
OsamaJa99 <osamajarrah111@gmail.com>
2025-08-13 13:14:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import About from "./Components/About";
+import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
